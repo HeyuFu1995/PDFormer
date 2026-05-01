4381479e83eb3b081ecf06d48f5955b2afe50066
Heyu Fu <heyufu2023@gmail.com>
2026-05-01 10:47:15 +1000

    Experiment 1 Setup: Using Physics info as direct input

--- libcity/model/traffic_flow_prediction/PDFormer.py
+++ libcity/model/traffic_flow_prediction/PDFormer.py
@@ -24,6 +24,8 @@ def drop_path(x, drop_prob=0., training=False):
 class TokenEmbedding(nn.Module):
     def __init__(self, input_dim, embed_dim, norm_layer=None):
         super().__init__()
+        getLogger().info("="*50)
+        getLogger().info(input_dim)
         self.token_embed = nn.Linear(input_dim, embed_dim, bias=True)
         self.norm = norm_layer(embed_dim) if norm_layer is not None else nn.Identity()
 
@@ -76,6 +78,7 @@ class DataEmbedding(nn.Module):
         self.embed_dim = embed_dim
         self.feature_dim = feature_dim
         self.value_embedding = TokenEmbedding(feature_dim, embed_dim)
+        self.ext_embedding = TokenEmbedding(feature_dim, embed_dim)
 
         self.position_encoding = PositionalEncoding(embed_dim)
         if self.add_time_in_day:
@@ -92,10 +95,11 @@ class DataEmbedding(nn.Module):
         x = self.value_embedding(origin_x[:, :, :, :self.feature_dim])
         x += self.position_encoding(x)
         if self.add_time_in_day:
-            x += self.daytime_embedding((origin_x[:, :, :, self.feature_dim] * self.minute_size).round().long())
+            x += self.daytime_embedding((origin_x[:, :, :, self.feature_dim + 1] * self.minute_size).round().long())
         if self.add_day_in_week:
-            x += self.weekday_embedding(origin_x[:, :, :, self.feature_dim + 1: self.feature_dim + 8].argmax(dim=3))
+            x += self.weekday_embedding(origin_x[:, :, :, self.feature_dim + 2: self.feature_dim + 9].argmax(dim=3))
         x += self.spatial_embedding(lap_mx)
+        x += self.ext_embedding(origin_x[:, :, :, self.feature_dim:self.feature_dim + 1])
         x = self.dropout(x)
         return x
 
